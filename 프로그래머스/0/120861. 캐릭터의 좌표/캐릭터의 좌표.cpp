06eac7beb611d24963f9b34557098ee82ce153e7
Dev_Means <32381357+KimMean@users.noreply.github.com>
2024-06-04 15:07:30 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> keyinput, vector<int> board) {
+    vector<int> answer(2, 0);
+    
+    for(string s : keyinput)
+    {
+        if(s == "up" && answer[1] < +board[1] / 2) answer[1]++;
+        else if (s == "down" && answer[1] > -board[1] / 2) answer[1]--;
+        else if (s == "left" && answer[0] > -board[0] / 2) answer[0]--;
+        else if (s == "right" && answer[0] < +board[0] / 2) answer[0]++;
+    }
+    return answer;
+}
