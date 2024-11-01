ba5f29e5405aaf2bcae07e886c18049eee1c2547
Dev_Means <32381357+KimMean@users.noreply.github.com>
2024-11-01 23:46:44 +0900

    [level 2] Title: 타겟 넘버, Time: 3.84 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -3,23 +3,26 @@
 
 using namespace std;
 
-//벡터, 정답횟수, 찾아야 하는 숫자, 들어간 깊이, 현재까지 합
-void dfs(vector<int>& numbers, int& answer, int target, int count = 0, int sum = 0) {
-    //마지막까지 순회했다면
-    if (count == numbers.size() - 1) {
-        //지금까지 더한값에 마지막 원소를 더할때 타겟과 같다면 카운트 증가
-        if (target == sum + numbers[count])        answer++;
-        //지금까지 더한값에 마지막 원소를 뺄때 타겟과 같다면 카운트 증가
-        if (target == sum - numbers[count])        answer++;
+int answer = 0;
+
+void dfs(vector<int>& numbers, int n, int sum, int target)
+{
+    if(n >= numbers.size())
+    {
+        // 마지막에 타겟넘버가 같다면 답 추가
+        if(sum == target)
+            answer++;
         return;
     }
-    //최대깊이까지 가지않았다면 더하거나 뺀상태로 탐색
-    dfs(numbers, answer, target, count + 1, sum + numbers[count]);
-    dfs(numbers, answer, target, count + 1, sum - numbers[count]);
+    
+    // + 경우
+    dfs(numbers, n+1, sum + numbers[n], target);
+    // - 경우
+    dfs(numbers, n+1, sum - numbers[n], target);
 }
 
 int solution(vector<int> numbers, int target) {
-    int answer = 0;
-    dfs(numbers, answer, target);
+    
+    dfs(numbers, 0, 0, target);
     return answer;
 }
