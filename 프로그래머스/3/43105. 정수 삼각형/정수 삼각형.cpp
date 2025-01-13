4c876805c22a8c0a182613ecf50466671be7b864
Dev_Means <32381357+KimMean@users.noreply.github.com>
2025-01-13 21:49:36 +0900

    [level 3] Title: 정수 삼각형, Time: 0.91 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -11,28 +11,26 @@ int solution(vector<vector<int>> triangle) {
     int length = triangle.size();
     dp[0][0] = triangle[0][0];
 
-    for (int i = 1; i < length; i++)
+    for(int i = 1; i < length; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for(int j = 0; j <= i; j++)
         {
-            if (j == 0)
-                dp[i][j] = dp[i - 1][j];
+            if(j == 0)
+                dp[i][j] = dp[i-1][j];
             else if (j == i)
-                dp[i][j] = dp[i - 1][j - 1];
+                dp[i][j] = dp[i-1][j-1];
             else
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]);
-
-            dp[i][j] += triangle[i][j];
+                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]);
             
+            dp[i][j] += triangle[i][j];
         }
     }
 
-    for (int i = 0; i < length; i++)
+    for(int i = 0; i < length; i++)
     {
-        if (dp[length-1][i] > answer)
+        if(dp[length-1][i] > answer)
             answer = dp[length-1][i];
     }
-
 
     return answer;
 }
